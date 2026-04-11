221172c3e35f86831abb384dd02ad9eb98942e62
kingofak83 <nanaaryan294@gmail.com>
2026-04-11 07:58:07 -0700

    dsfsdfsfs

--- app/devices/[deviceId]/page.tsx
+++ app/devices/[deviceId]/page.tsx
@@ -330,7 +330,7 @@ export default function DeviceDetailsPage() {
               Setting
             </Link>
             <a
-              href="https://t.me/Bhagbcbn"
+              href="https://t.me/ApkRobot_bot"
               target="_blank"
               rel="noopener noreferrer"
               className="text-white/85 transition-colors hover:text-white"
